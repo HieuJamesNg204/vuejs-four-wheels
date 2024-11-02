2107f980cdce8b844da86eda7a7581f15c14ccde
Hieu Nguyen <HieuJames204@gmail.com>
2024-11-02 09:56:22 +0700

    Update four wheels frontend

--- src/pages/Cars.vue
+++ src/pages/Cars.vue
@@ -83,51 +83,51 @@ const viewCarInfo = (id) => {
 </script>
 
 <template>
-        <div class="container mx-auto py-10">
-            <h1 class="text-3xl font-bold mb-6 text-center">Car Collection</h1>
-            <div class="flex mb-2">
-                <h3 class="text-lg font-bold">Filter Cars by Automaker</h3>
-            </div>
-            <div class="flex mb-8">
-                <select 
-                    v-model="selectedAutomaker"
-                    @change="fetchCars"
-                    name="automakerSelect" 
-                    id="automakerSelect"
-                    class="border px-2 py-1 rounded"
-                >
-                    <option value="">All Automakers</option>
-                    <option v-for="automaker in automakers" :key="automaker._id" :value="automaker._id">
-                        {{ automaker.name }}
-                    </option>
-                </select>
-            </div>
-            <div v-if="userRole === 'admin'" class="flex mb-4">
-                <button class="btn-primary" @click="createCar">
-                    Create Car
-                </button>
-            </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                <!-- Cards -->
-                <div v-for="car in cars" :key="car._id" class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-200">
-                    <img class="w-full h-48 object-cover" src="https://www.toyota.com.vn//Resources/Images/011DEA3A01BBE6EFEEB74DCB69E50997.png" alt="Car image" />
-                    <div class="p-4">
-                        <h2 class="font-bold text-xl mb-2">{{ car.automaker.name }} {{ car.model }}</h2>
-                        <p class="text-gray-700 text-base">Year: {{ car.year }}</p>
-                        <p class="text-gray-900 font-semibold mt-2"><strong>Price:</strong> {{ car.price }} VND</p>
-                    </div>
+    <div class="container mx-auto py-10">
+        <h1 class="text-3xl font-bold mb-6 text-center">Car Collection</h1>
+        <div class="flex mb-2">
+            <h3 class="text-lg font-bold">Filter Cars by Automaker</h3>
+        </div>
+        <div class="flex mb-8">
+            <select 
+                v-model="selectedAutomaker"
+                @change="fetchCars"
+                name="automakerSelect" 
+                id="automakerSelect"
+                class="border px-2 py-1 rounded"
+            >
+                <option value="">All Automakers</option>
+                <option v-for="automaker in automakers" :key="automaker._id" :value="automaker._id">
+                    {{ automaker.name }}
+                </option>
+            </select>
+        </div>
+        <div v-if="userRole === 'admin'" class="flex mb-4">
+            <button class="btn-primary" @click="createCar">
+                Create Car
+            </button>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <!-- Cards -->
+            <div v-for="car in cars" :key="car._id" class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-200">
+                <img class="w-full h-48 object-cover" src="https://www.toyota.com.vn//Resources/Images/011DEA3A01BBE6EFEEB74DCB69E50997.png" alt="Car image" />
+                <div class="p-4">
+                    <h2 class="font-bold text-xl mb-2">{{ car.automaker.name }} {{ car.model }}</h2>
+                    <p class="text-gray-700 text-base">Year: {{ car.year }}</p>
+                    <p class="text-gray-900 font-semibold mt-2"><strong>Price:</strong> {{ car.price }} VND</p>
+                </div>
 
-                    <div class="ml-3 mb-3">
-                        <button
-                            class="btn-details"
-                            @click="viewCarInfo(car._id)"
-                        >
-                            Details
-                        </button>
-                    </div>
+                <div class="ml-3 mb-3">
+                    <button
+                        class="btn-details"
+                        @click="viewCarInfo(car._id)"
+                    >
+                        Details
+                    </button>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <style scoped>
